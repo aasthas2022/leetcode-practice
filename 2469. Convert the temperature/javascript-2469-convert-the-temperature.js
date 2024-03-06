2593f6bc0c262f1455d47412e231ce0bbd7bcc31
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-03-05 19:09:51 -0500

    2469-convert-the-temperature

---
+++ 2469. Convert the temperature/javascript-2469-convert-the-temperature.js
@@ -0,0 +1,14 @@
+const CELCIUS_TO_KELVIN =   
+const CELCIOUs_TO_FARHENITE = 32
+
+function convertTemperature(celcius) {
+    let celcius_to_kelvin = celcius + CELCIUS_TO_KELVIN
+    let celcius_to_farhaneiet = celcius * 1.8 + CELCIOUs_TO_FARHENITE
+    return [formattedTemp(celcius_to_kelvin), formattedTemp(celcius_to_farhaneiet)]
+}
+
+function formattedTemp(value) {
+    return Number(value.toFixed(5))   
+}
+
+console.log(convertTemperature(36.50))
