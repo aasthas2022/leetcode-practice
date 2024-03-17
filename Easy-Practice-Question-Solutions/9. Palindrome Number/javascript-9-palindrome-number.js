980b203f901ee1abd244e0ae1cd7b01ad7ef5c1f
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-03-17 13:15:46 -0400

    palindrome-number

---
+++ Easy-Practice-Question-Solutions/9. Palindrome Number/javascript-9-palindrome-number.js
@@ -0,0 +1,8 @@
+var isPalindrome = function(x) {
+    if (x.toString() == x.toString().split("").reverse().join("")){
+        return true
+    }
+    return false
+};
+
+console.log(isPalindrome(121))
